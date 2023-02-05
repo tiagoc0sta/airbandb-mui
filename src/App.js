514a178d06fed8fe2c0fa625b4e70119dfb3c8af
Tiago Costa <tiagodpv@gmail.com>
2023-02-04 23:51:25 -0500

    add array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from 'components/Header';
+
+import Container from '@mui/material/Container';
+
+import React from 'react';
 import './App.css';
+
+import { displayOnDesktop } from 'themes/commonStyles';
+import OptionsTab from 'components/OptionsTab';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh',
+        }}
+      >
+        <Box>
+          <Header />
+          <OptionsTab />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: 1,
+            height: 100,
+            overflowY: 'scroll',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Container maxWidth='xl' sx={{ mb: 3 }}>
+            <Box
+              sx={{
+                display: { xs: 'flex', md: 'none' },
+              }}
+            ></Box>
+          </Container>
+        </Box>
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}></Box>
+        <Box sx={displayOnDesktop}></Box>
+      </Box>
+    </React.Fragment>
   );
 }
 
